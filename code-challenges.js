055149bc446cd1148bd68135d851a2f3e9b39225
Marc Matias <mrcallenmatias@gmail.com>
2022-02-22 09:42:20 -0800

    completed assessment

--- code-challenges.js
+++ code-challenges.js
@@ -4,16 +4,6 @@
 
 // Pseudo coding is required.
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in any two strings and returns the string with more characters. Use the two sets of test variables provided.
-
-// Set one:
-const fruit1 = "apple"
-const fruit2 = "banana"
-
-// Set two:
-const fruit3 = "cherry"
-const fruit4 = "kiwi"
-
 
 // --------------------1) Create a function that takes in any given number and determines if the number is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit. Use the test variables provided below. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
@@ -21,22 +11,77 @@ const temp1 = 42
 const temp2 = 350
 const temp3 = 212
 
+// INPUT
+// create a funtion that takes a parameter
+// use conditional statement to set the degrees for each number
+// if number is 42, then it is below boiling point
+// if is it 212, the its is at boiling point
+// 350 is condidered above boiling point
+
+// OUTPUT
+// create a console log and use function that's been created
+// use the const temperature to use as parameters
+
+const tempNumber = (temperature) => {
+    if (temperature === 42) {
+        return `${temperature} is below boiling point`
+    } else if (temperature === 350) {
+        return `${temperature} is above boiling point`
+    } else {
+        return `${temperature} is at boiling point`
+    }
+}
+console.log(tempNumber(temp1));
+console.log(tempNumber(temp2));
+console.log(tempNumber(temp3));
 
 // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
 
 const myNumbers1 = [3, 7, 0, 36, -9]
 const myNumbers2 = [8, -7, 42, 9, 13]
 
+// INPUT
+// create a variable and use the method concat to combine two given arrays
 
-// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
+// OUTPUT
+// input console log to see the result the of combine array
+// finnally, make another log and use .lenth to see the length of the combine array
+
+var number = myNumbers1.concat(myNumbers2)
+console.log(number);
+console.log(number.length)
+
+
+// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "2202 ahplA"
 
 const currentCohort = "Alpha 2022"
+
+// INPUT
+// use split method to turn string into array
+// in array form, reverse will be accesible to use reversing the string in an array form
+// then use join to turn array back to string
+
+// OUPUT
+// console log should show the final answer into the terminal
+
+console.log(currentCohort.split("").reverse().join(""));
 
 
 // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
 const myArray = [13, 34, 5, 10, 27, 42]
 
+for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] % 2 === 0) {
+        console.log("even")
+    } else {
+        console.log("odd");
+    }
+}
+
+// create a loop statement in order to go through an array
+// then use conditional statement to know whether the number is odd or even
+// by using % sign, conditional statement will be able to tell if its odd or even
 
 // --------------------5) Create a function that takes in a set of any two numbers and subtracts the smaller number from the larger number using the sets of test variables provided below. Expected output: 42, 3
 
@@ -47,3 +92,23 @@ const number2 = 100
 // Set two:
 const number3 = 27
 const number4 = 24
+
+const sub = (num1, num2) => {
+    var minNumber = num1;
+    var maxNnumber = num2;
+    if (minNumber > num2) {
+        minNumber = num2
+        maxNnumber = num1
+    }
+    return maxNnumber - minNumber
+}
+console.log(sub(number1, number2))
+console.log(sub(number3, number4))
+
+// INPUT
+// make s function that takes 2 parameter
+// make 2 variables, one that is minimum and another a maximum number.
+// use conditional statement to get bigger number 
+// then subtract small number from bigger number
+// OUTPUT
+// the output should be both positive number since we make s function that makes smaller number be subtracted from bigger number.
--- interview-questions.md
+++ interview-questions.md
@@ -5,51 +5,43 @@ First, without external resources. Challenge yourself to answer from memory as i
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-INSTRUCTOR EXAMPLE: What is a conditional statement?
-
-  Your answer:
-
-  Researched answer:
-
-
-
 1. What is git? What is the difference between git and Github?
 
-  Your answer:
+  Your answer: I think git is the bridge to transfer files from your computer to the web. And github is like your journal of programs and files of codes you have made.
 
-  Researched answer:
+  Researched answer: Git is a support programmer that helps programmers with speed, data integrity and distribution of programs from one place to another. The difference between git and github is that git is a tool use to manage source code while github is an online server where programmers can connect and upload their work.
 
 
 
 2. Which JavaScript operators will return a Boolean value?
 
-  Your answer:
+  Your answer: typeOf
 
-  Researched answer:
+  Researched answer: || (or sign) and && (and sign) return the Boolean value.
 
 
 
 3. What is an index? What is the difference between index and value?
 
-  Your answer:
+  Your answer: Index is the numbering system of the computer that starts at 0 instead of 1.
 
-  Researched answer:
+  Researched answer: Index is a numberical position in an array. If there are 10 positions inside the array, then the index starts from 0-9.
 
 
 
 4. What is iteration?
 
-  Your answer:
+  Your answer: Iteration is the repetition of something, also called loop. 
 
-  Researched answer:
+  Researched answer: Iteration is the repetition of a process in the programming language.
 
 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer:Two programmers helping each other to finish a given task. There is the driver and the navigator role.
 
-  Researched answer:
+  Researched answer: Consists of two programmers sharing the same computer or workstation to finish a task. The driver is usually the person in the computer typhing and the navigator is the person checking errors and telling the driver the next step to the task. The pair swaps every few minutes to balance the task.
 
 
 
@@ -57,12 +49,12 @@ INSTRUCTOR EXAMPLE: What is a conditional statement?
 
 Research and define the following terms to the best of your ability.
 
-1. Higher Order Functions:
+1. Higher Order Functions: A function that accepts another funtion as parameter.
 
-2. Jest:
+2. Jest: It is a framework for javascript to ensure the correctness of programming.
 
-3. Objects:
+3. Objects: Is a container for properties in programming. 
 
-4. Method:
+4. Method: Method is a function that is stored inside and object.
 
-5. Classes:
+5. Classes: It is has a little similarity to function that includes data and functions that can manipulate data.
